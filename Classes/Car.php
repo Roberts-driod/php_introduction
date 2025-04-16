4c593d8383cac9e90488f05d6614f54271778294
ErnestsGriezis <ernestsgg@gmail.com>
2025-04-16 18:46:07 +0300

    Constructors in OOP  PHP

--- Classes/Car.php
+++ Classes/Car.php
@@ -6,5 +6,14 @@ class Car
     private $brand;
     private $color;
     private $vehicleType = "car";
+
+    // Constructor
+    public function __construct($brand, $color)
+    {
+        $this->brand = $brand;
+        $this->color = $color;
+    }
 }
 
+$car01 = new Car("Volvo", "green");
+$car02 = new Car("BMW", "blue");
